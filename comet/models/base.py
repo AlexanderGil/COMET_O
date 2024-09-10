4c26dab5fcfe10f991b3aac877c58edc20e966b7
Alex G <93571405+AlexanderGil@users.noreply.github.com>
2024-09-10 13:13:43 +0700

    Update base.py

--- comet/models/base.py
+++ comet/models/base.py
@@ -119,8 +119,7 @@ class CometModel(ptl.LightningModule, metaclass=abc.ABCMeta):
         self.save_hyperparameters()
         
         self.encoder = str2encoder[self.hparams.encoder_model].from_pretrained(
-                self.hparams.pretrained_model,
-                load_pretrained_weights=self.hparams.load_pretrained_weights,
+            self.hparams.pretrained_model, load_pretrained_weights
         )
         
         self.epoch_nr = 0
